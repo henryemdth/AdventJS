a39bfef5b9906a230f8a8b6deb2bb021e39f2150
henryemdth <henry.emdth@gmail.com>
2025-12-17 16:41:07 -0400

    add problem 17

---
+++ 2025/17.js
@@ -0,0 +1,20 @@
+function hasFourLights(board) {
+  if(!board?.length) return false;
+    const patternGreen = /G{4,}/g;
+    const patternRed = /R{4,}/g;
+    return board.some(row => {
+        const value = row.join('');
+        return patternGreen.test(value) || patternRed.test(value);
+    }) || (board[0] ?? []).map((char, index) =>
+        board.map(array => array[index]).join('')
+    ).some(col => patternGreen.test(col) || patternRed.test(col));
+}
+// 8
+console.log(
+    hasFourLights([
+        ['G', 'G', 'G', 'd', 'G'],
+        ['.', 'G', '.', '.'],
+        ['.', 'G', '.', '.'],
+        ['.', 'G', '.', '.']
+    ])
+);
